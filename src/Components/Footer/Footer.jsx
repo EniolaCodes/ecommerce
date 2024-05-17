4de3045719c0d1be3a2d7f7420597f402320e8a3
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-05-17 17:12:10 +0100

    add the ShopCategory page

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => {
 			</div>
 			<div className="footer-copyright">
 				<hr />
-				<p>Copyright @ 2024 - All Right Reserved</p>
+				<small>&#169; Copyright 2024 - All Right Reserved</small>
 			</div>
 		</div>
 	);
