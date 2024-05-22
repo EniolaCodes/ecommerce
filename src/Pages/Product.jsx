96497059f582590e0d16f7591b17df19b9ce5cf5
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-05-22 12:07:12 +0100

    add the ProductDisplay component

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../Context/ShopContext";
+import BreadCrum from "../Components/BreadCrums/BreadCrum";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 
 const Product = () => {
-	return <div></div>;
+	const { all_product } = useContext(ShopContext);
+	const { productId } = useParams();
+	const product = all_product.find((e) => e.id === Number(productId));
+	return (
+		<div className="">
+			<BreadCrum product={product} />
+			<ProductDisplay product={product} />
+		</div>
+	);
 };
 
 export default Product;
